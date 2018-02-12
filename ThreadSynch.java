faff9646dd70cb1a4870fae1c3b656ee6691ec23
Clarence Tso <clarence.tso.2@gmail.com>
2018-02-12 16:49:32 -0600

    finished 1a

--- ThreadSynch.java
+++ ThreadSynch.java
@@ -5,14 +5,45 @@
 import java.util.concurrent.Semaphore; // for implementation using Semaphores
 
 public class ThreadSynch {
+	private int parties;
+	private Semaphore lock;
+	private int numWaiting;
+	private Semaphore block;
 	
 	public ThreadSynch(int parties) {
+		this.parties = parties;
+		
+		sem = new Semaphore(parties); //keeps track of waiting
+		
+		block = new Semaphore(0);
+		lock = new Semaphore(1);
+		
+		numWaiting = 0;
+		
+		
+		
 	}
 	
 	public int await() throws InterruptedException {
-           int index = 0;
-		
-          // you need to write this code
+		// Waits until all parties have invoked await on this ThreadSynch.
+		// If the current thread is not the last to arrive then it is
+		// disabled for thread scheduling purposes and lies dormant until the last thread arrives.
+		// Returns: the arrival index of the current thread, where index
+		// (parties - 1) indicates the first to arrive and zero indicates
+		// the last to arrive.
+		int index = 0;
+   		lock.acquire();
+   		numWaiting++;
+   		index = parties - numWaiting;
+   		if(index == 0){
+   			block.release(parties);
+			numWaiting=0;
+   		}
+
+   		lock.release();
+   		block.acquire();
+           
+        // you need to write this code
 	    return index;
 	}
 }
