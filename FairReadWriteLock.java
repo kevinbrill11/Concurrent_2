5d7cb362ee1bb53d7f925cb40472c0e8a0cdd1a3
Kevin Brill <kevinjbrill@utexas.edu>
2018-02-14 16:57:51 -0600

    start fixing Q2

--- FairReadWriteLock.java
+++ FairReadWriteLock.java
@@ -1,15 +1,23 @@
 //TESTING
+import java.util.Queue;
 import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class FairReadWriteLock {
 	public int numReaders, numWriters;
-	public static int timestamp=0;
+	public static Queue<Boolean> waiting;
+	//public static int timestamp=0;
 	public int myStamp;
 	public int turn = 0;
-                        
+    ReentrantLock readerlock = new ReentrantLock();
+	ReentrantLock writerlock = new ReentrantLock();
+	/* READER = 0
+	 * WRITER = 1
+	 */
 	public synchronized void beginRead() {
-		timestamp++;
-		myStamp = timestamp;
+		//timestamp++;
+		//myStamp = timestamp;
+		//addToQueue()
 		numReaders++;
 		while(myStamp > timestamp){
 			/********WAIT CALL*********/
@@ -29,8 +37,8 @@ public class FairReadWriteLock {
 	}
 	
 	public synchronized void beginWrite() {
-		timestamp++;
-		myStamp = timestamp;
+		//timestamp++;
+		//myStamp = timestamp;
 		numWriters++;
 		while((numReaders != 0 ) && (myStamp > turn)){ //to account for the recent increase in numWriters
 			/********WAIT CALL*********/
@@ -49,9 +57,8 @@ public class FairReadWriteLock {
 		
 	}
 	
-//	private synchronized int getTimestamp(){
-//		timestamp++;
-//		return timestamp;	//return thread's local timestamp
-//	}
+	private synchronized void addToQueue(boolean val){
+		waiting.add(val);
+	}
 	
 }
